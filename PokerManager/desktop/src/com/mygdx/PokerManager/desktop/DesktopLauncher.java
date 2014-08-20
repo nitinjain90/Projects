27078e21bc1f574323b237e89f1fcda7d0c09bfc
Nitin Jain <nitin@crispygam.es>
2014-08-20 19:12:30 +0530

    latest

---
+++ PokerManager/desktop/src/com/mygdx/PokerManager/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.PokerManager.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.PokerManager.PokerManager;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PokerManager(), config);
+	}
+}
